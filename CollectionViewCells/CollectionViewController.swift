8f8def258921267241641dedfae59a1e8dcc11b3
Daniel Martínez <dmartinez@danielmartinez.info>
2015-08-26 15:09:10 +0200

    Added Search bar

--- CollectionViewCells/CollectionViewController.swift
+++ CollectionViewCells/CollectionViewController.swift
@@ -8,17 +8,80 @@
 
 import UIKit
 
-class CollectionViewController: UICollectionViewController {
+class CollectionViewController: UICollectionViewController, UISearchResultsUpdating, UISearchBarDelegate {
 
+    @IBOutlet weak var searchbarView: UIView!
+    
+    var resultSearchController = UISearchController()
+    var searchButton: UIBarButtonItem!
+    
     private let reuseIdentifier = "cellIdentifier"
     
+    var items: [String]!
+    var filteredItems: [String]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initializeItems()
+        setupSearchBar()
+    }
+    
+    func initializeItems() {
+        items = []
+        for (var i = 0; i < 40; i++) {
+            items.append("Hello\(i)")
+        }
+        filteredItems = items
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        self.navigationItem.titleView = nil
+        self.navigationItem.rightBarButtonItem = searchButton
+    }
+    
+    func showSearch() {
+        self.navigationItem.rightBarButtonItem = nil
+        self.navigationItem.titleView = resultSearchController.searchBar
+        resultSearchController.searchBar.becomeFirstResponder()
+    }
+    
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        
+        let searchText = searchController.searchBar.text
+        
+        if ((searchText == nil) || (searchText == "")) {
+            filteredItems = items
+        } else {
+            filteredItems = items.filter { $0.rangeOfString(searchText) != nil }
+        }
+        
+        self.collectionView!.reloadData()
+        
+    }
+    
+    func setupSearchBar() {
+        searchButton = UIBarButtonItem(barButtonSystemItem: .Search, target: self, action: "showSearch")
+        navigationItem.rightBarButtonItem = searchButton
+        self.resultSearchController = {
+            let controller = UISearchController(searchResultsController: nil)
+            controller.searchResultsUpdater = self
+            controller.dimsBackgroundDuringPresentation = false
+            controller.hidesNavigationBarDuringPresentation = false  // default true
+            controller.searchBar.sizeToFit()
+            return controller
+            }()
+        self.resultSearchController.searchBar.delegate = self
+    }
+    
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 40
+        return filteredItems.count
     }
 
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! CircleWithNotificationCollectionViewCell
+        
+        let item: String = self.filteredItems[indexPath.row]
         
         cell.imageView.layer.cornerRadius = cell.imageView.frame.size.width/2
         cell.imageView.clipsToBounds = true
@@ -32,7 +95,7 @@ class CollectionViewController: UICollectionViewController {
         cell.notificationIcon.text = "99999999"
         cell.notificationIcon.layer.cornerRadius = 5
         
-        cell.locationName.text = "Johannesburg"
+        cell.locationName.text = item
         
         var blurEffect = UIBlurEffect(style: UIBlurEffectStyle.Dark)
         var blurEffectView = UIVisualEffectView(effect: blurEffect)
